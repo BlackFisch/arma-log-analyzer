ca80f584b10fabd50b8f3579cbb0da3bda070a88
Rico Goldhardt <blackfisch@outlook.com>
2021-11-04 16:08:53 +0100

    debug 7

--- start.py
+++ start.py
@@ -4,7 +4,8 @@ from gevent.pywsgi import WSGIServer
 print('Hello Docker')
 print(app)
 
-http_server = WSGIServer(('localhost', 8000), app)
-print('Started Webserver')
-# http_server.serve_forever()
-http_server.start()
+if __name__ == '__main__':
+    http_server = WSGIServer(('localhost', 8000), app)
+    print('Started Webserver')
+    http_server.serve_forever()
+    # http_server.start()
