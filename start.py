36ea5ebd121dd617c22d401ee4af0f8c365ffa60
Rico Goldhardt <blackfisch@outlook.com>
2021-11-04 15:55:01 +0100

    debug 4

--- start.py
+++ start.py
@@ -1,4 +1,9 @@
 from app import app
+from gevent.pywsgi import WSGIServer
 
 print('Hello Docker')
 print(app)
+
+http_server = WSGIServer(('localhost', 8000), app)
+print('Started Webserver')
+http_server.serve_forever()
